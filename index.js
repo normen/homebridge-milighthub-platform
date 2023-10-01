6f25fc768423934610534f077f99402d9089d294
Zer0x00 <buenyamin.olgun@gmail.com>
2023-10-01 16:40:56 +0200

    Performance: Increase speed of ColorTemperature calculation by ~20%

--- index.js
+++ index.js
@@ -772,21 +772,29 @@ function RGBtoHueSaturation(r, g, b) {
 
 function HomeKitColorTemperatureToHueSaturation(ColorTemperature) {
   const dKelvin = 10000 / ColorTemperature;
-  const rgb = [
-    dKelvin > 66 ? 351.97690566805693 + 0.114206453784165 * (dKelvin - 55) - 40.25366309332127 * Math.log(dKelvin - 55) : 255,
-    dKelvin > 66 ? 325.4494125711974 + 0.07943456536662342 * (dKelvin - 50) - 28.0852963507957 * Math.log(dKelvin - 55) : 104.49216199393888 * Math.log(dKelvin - 2) - 0.44596950469579133 * (dKelvin - 2) - 155.25485562709179,
-    dKelvin > 66 ? 255 : 115.67994401066147 * Math.log(dKelvin - 10) + 0.8274096064007395 * (dKelvin - 10) - 254.76935184120902
-  ].map(v => Math.max(0, Math.min(255, v)) / 255);
-  const max = Math.max(...rgb);
-  const min = Math.min(...rgb);
+
+  let r = (dKelvin > 66) ? 351.97690566805693 + 0.114206453784165 * (dKelvin - 55) - 40.25366309332127 * Math.log(dKelvin - 55) : 255;
+  let g = (dKelvin > 66) ? 325.4494125711974 + 0.07943456536662342 * (dKelvin - 50) - 28.0852963507957 * Math.log(dKelvin - 55) : 104.49216199393888 * Math.log(dKelvin - 2) - 0.44596950469579133 * (dKelvin - 2) - 155.25485562709179;
+  let b = (dKelvin > 66) ? 255 : 115.67994401066147 * Math.log(dKelvin - 10) + 0.8274096064007395 * (dKelvin - 10) - 254.76935184120902;
+
+  r = Math.max(0, Math.min(255, r)) / 255;
+  g = Math.max(0, Math.min(255, g)) / 255;
+  b = Math.max(0, Math.min(255, b)) / 255;
+
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
   const d = max - min;
+
   let h = 0;
   const s = max ? 100 * d / max : 0;
+
   if (d) {
-    switch (max) {
-      case rgb[0]: h = (rgb[1] - rgb[2]) / d + (rgb[1] < rgb[2] ? 6 : 0); break;
-      case rgb[1]: h = (rgb[2] - rgb[0]) / d + 2; break;
-      default: h = (rgb[0] - rgb[1]) / d + 4; break;
+    if (max === r) {
+      h = (g - b) / d + (g < b ? 6 : 0);
+    } else if (max === g) {
+      h = (b - r) / d + 2;
+    } else {
+      h = (r - g) / d + 4;
     }
     h *= 60;
   }
